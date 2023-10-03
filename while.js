ec6b2b66ba672e9ac7b31c9cb5facc37c5d44639
ekramhossainfahad <ehfahad96@gmail.com>
2023-10-03 11:33:28 +0600

    first commit

---
+++ while.js
@@ -0,0 +1,12 @@
+// var roastGiven = 0;
+// while(roastGiven <= 7){
+//     console.log('Roast den, please!!!');
+//     console.log(roastGiven);
+//     roastGiven++;
+// }
+
+var riceGiven = 0;
+while(riceGiven <= 10){
+    console.log('Rice den, Please!!!');
+    riceGiven++;
+}
