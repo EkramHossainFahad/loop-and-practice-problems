76329784e0a7e8680e1987e94fdef77feb51d7c4
ekramhossainfahad <ehfahad96@gmail.com>
2023-10-03 11:56:59 +0600

    2nd commit

---
+++ even.js
@@ -0,0 +1,14 @@
+// Target: 1 to 10 all even numbers
+var number = 0;
+while(number <= 10){
+    console.log(number);
+    number = number + 2;
+}
+
+
+// Target: 1 to 20 all even numbers
+var num = 0;
+while(num <= 20){
+    console.log(num);
+    num = num + 2;
+}
